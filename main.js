70bff33c644e8f57ee427dad3bcc1571f96d8ecd
Chris Donofrio <cdonofrio20@gmail.com>
2016-01-27 13:31:18 -0500

    hangman node game finally functional!

---
+++ main.js
@@ -0,0 +1,52 @@
+var prompt = require('prompt');
+var Word = require('/word.js');
+
+prompt.start();
+
+var game = {
+	wordBank: ['Leonardo', 'Donatello', 'Michelangelo', 'Raphael'];
+	guessesRemaining: 10;
+	currentWrd: null;
+	startGame: function() {
+		num = Math.floor(Math.random() * (4 - 1) + 1)
+		var word = new Word(this.wordBank[num]);
+		this.currentWrd = word;
+		this.currentWrd.getLets();
+		this.keepPromptingUser();
+
+	},
+	keepPromptingUser: function() {
+		var self = this;
+		prompt.get(["guessLetter"], function(err, result) {
+			console.log(“The Letter or space you guessed is ” + result.guessLetter);
+			var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
+			if (findHowManyOfUserGuess === 0) {
+				console.log("You guessed wrong!");
+				self.guessesRemaining -= 1;
+			}
+			else {
+				console.log("You guessed correctly!");
+				if (self.currentWrd.didWeFindTheWord() === true) {
+					console.log("You won!");
+					return 1;
+				}
+				else {
+					console.log("guessesRemaining:" + self.guessesRemaining);
+					console.log(self.currentWrd.wordRender());
+				}
+			}
+			if (self.guessesRemaining > 0 && self.currentWrd.found === false) {
+				self.keepPromptingUser();
+			}
+			else if(self.guessesRemaining === 0) {
+				console.log("Game over bro!");
+				console.log("The correct word was " + self.currentWrd.word);
+			}
+			else {
+				console.log(self.currentWrd.wordRender());
+		    }
+    	});
+  	}
+};
+
+game.startGame();
---
+++ word.js
@@ -0,0 +1,43 @@
+var Letter = require("./letter.js");
+
+var Word = function(wrd) {
+	this.word = wrd;
+	this.lets = [];
+	this.found = false;
+	this.getLets = function() {
+		for (i = 0; i < this.word.length; i++) {
+			letter = this.word.charAt(i);
+			var let = new Letter(letter);
+			this.lets.push(let);
+		}
+	}
+
+this.checkIfLetterFound = function(guessLetter) {
+	whatToReturn = 0;
+	for (i = 0; i < this.lets.length; i++) {
+		if (this.lets[i].charac === guessLetter) {
+			this.lets[i].appear = true;
+			whatToReturn += 1
+		}
+	}
+		return whatToReturn;
+}
+
+this.didWeFindTheWord = function() {
+	if(this.lets.every(function(curLet) {
+		return curLet.appear === true;
+		}) === true) {
+		return true;
+	}
+}
+
+this.wordRender = function(){
+    var str = "";
+    for (var i = 0; i < this.lets.length; i++){
+      str += this.lets[i].letterRender(this.lets[i]);
+    }
+    return str;
+  }
+}
+
+module.exports = Word;
